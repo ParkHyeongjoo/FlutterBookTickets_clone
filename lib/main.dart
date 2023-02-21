fd997133ee159186fef15c7053a4ea7c86c59c30
hyeongjooPark <hyeongjoopark0324@gmail.com>
2023-02-21 19:38:02 +0900

    HomeScreen, SearchScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookticketsclone/screens/bottom_bar.dart';
+import 'package:bookticketsclone/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar()
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,11 @@
+import 'package:bookticketsclone/screens/ticket_view.dart';
+import 'package:bookticketsclone/utils/app_info_list.dart';
+import 'package:bookticketsclone/widgets/double_text_widget.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../utils/app_styles.dart';
+import 'hotel_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,21 +13,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFeeedf2),
+      backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('Good Morning'),
-                        const Text('Book Tickets'),
+                        Text('Good Morning', style: Styles.headLineStyle3,),
+                        const Gap(5),
+                        Text('Book Tickets', style: Styles.headLineStyle1,),
                       ],
                     ),
                     Container(
@@ -37,10 +46,47 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ],
-                )
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      Text("Search", style: Styles.headLineStyle4,),
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                AppDoubleTextWidget(bigText: 'Upcoming Flights', smallText: 'View all'),
               ],
             ),
-          )
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: ticketList.map((ticket) => TicketView(ticket: ticket)).toList()
+              ),
+            ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: AppDoubleTextWidget(bigText: 'Hotels', smallText: 'View all',),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: hotelList.map((hotel) => HotelScreen(hotel: hotel)).toList()
+              ),
+          ),
         ],
       ),
     );
